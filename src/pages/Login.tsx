2ed261e2f90f293e2f260b2e2ef1d26b827c3efb
Cecep Nandang <cecep@kima.one>
2025-07-15 23:56:05 +0700

    fix: login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,7 +19,7 @@ const Login = () => {
     setLoading(true);
     
     try {
-      const response = await axios.post('https://api-inventory.isavralabel.com/api/patiens/login', {
+      const response = await axios.post('https://api-inventory.isavralabel.com/api/patients/login', {
         username,
         password
       });
